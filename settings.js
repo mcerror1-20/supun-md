90d2259c24c0774d36b6bb8faf923d5bbd0e428a
MC-Error <mcerror310@gmail.com>
2025-12-25 10:20:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'SUPUN-MD=adFFGZSR#0bTUWR14Wx_6BlvM9-5A3NPlrA2aMbuvsQBaAX65BQM' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'SUPUN-MD=d3b3be5d-2029-413f-a7ca-a9578f67d1f4' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 SUDO: process.env.SUDO === undefined ? '94718461889' : process.env.SUDO,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
